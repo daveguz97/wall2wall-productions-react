753fe3d6b93b9b8f1a84eda2c1c1c06ce137dfd5
David Guzman <davidguzmaninc@gmail.com>
2021-10-01 00:42:10 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
+import './App.scss';
 
 function App() {
+  const boxRef = useRef();
+  // wait until DOM has been rendered
+  useEffect(() => {
+    gsap.to(boxRef.current, { rotation: "+=360" });
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4 className="box" ref={boxRef}>Hi</h4>
     </div>
   );
 }
